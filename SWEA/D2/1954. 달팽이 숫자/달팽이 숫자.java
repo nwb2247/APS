3400cc687dfa5a0111d6c09ebc63f649bdc54a87
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-04-02 12:11:37 +0900

    [D2] Title: 달팽이 숫자, Time: 78 ms, Memory: 24,192 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,52 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Solution {
 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int T = Integer.parseInt(br.readLine());
 		
 		for (int t=1; t<=T; t++) {
 			int N = Integer.parseInt(br.readLine());
+			
 			int[][] map = new int[N][N];
 			
-			int K = N; // 같은 방향으로 연달아 몇번 입력할 것인지
-			int dir = 1; // 해당 방향에 대한 증감을 의미
+			int[] dr = {0,1,0,-1};
+			int[] dc = {1,0,-1,0};
 			
-			int r=0, c=-1;
-			int num = 1; 
-			while (K >= 0) { // 다른 여러 종료 조건도 가능 (num <= N*N-1 등)
+			int d = 0;
+			int c = 0;
+			int r = 0;
+			
+			for (int i=1; i<=N*N; i++) {
 				
-				for (int k=1; k<=K; k++) { // k는 현재방향에서 더해진 횟수를 의미
-					c += dir;
-					map[r][c] = num++;
+				map[r][c] = i;
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				if (nr >= N || nr < 0 || nc >= N || nc < 0 || map[nr][nc] != 0) {
+					d = (d+1)%4;
+					nr = r + dr[d];
+					nc = c + dc[d];
 				}
-				
-				K--;
-				
-				for (int k=1; k<=K; k++) {
-					r += dir;
-					map[r][c] = num++;
-				}
-				
-				dir = -dir;
+				r = nr;
+				c = nc;
 			}
 			
-			StringBuilder sb;
-			System.out.println("#"+t);
-			
-			for (r=0; r<N; r++) {
-				sb = new StringBuilder();
-				for (c=0; c<N; c++) {
-					sb.append(map[r][c] + " ");
+			StringBuilder sb = new StringBuilder();
+			sb.append("#"+t+"\n");
+			for (int i=0; i<N; i++) {
+				for (int j=0; j<N; j++) {
+					sb.append(map[i][j]+ " ");
 				}
-				System.out.println(sb.toString());
+				sb.append("\n");
 			}
+			
+			System.out.println(sb.toString().trim());
+			
 			
 		}
+		
 
 	}
 
