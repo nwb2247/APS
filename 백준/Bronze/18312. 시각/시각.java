4491621e93a52e49e0976e5a19c83555fd4a8ea1
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-04-02 11:24:02 +0900

    [Bronze II] Title: 시각, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/18312. 시각/시각.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int count=0;
+        for(int h=0; h<=N; h++) {
+            if (h % 10 == K || h / 10 == K) {
+                count += 60*60;
+            } else {
+                for (int m=0; m<60; m++) {
+                    if (m % 10 == K || m / 10 == K) {
+                        count += 60;
+                    } else {
+                        for (int s=0; s<60; s++) {
+                            if (s % 10 == K || s / 10 == K) {
+                                count += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
