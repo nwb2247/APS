175bafa5841c0a7c18ecba15e160fd2614fd7aa4
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-04-07 15:34:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 83 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -30,34 +30,34 @@ public class Solution {
 			
 			outer : 
 			for (int r=0; r<N; r++) {
-				int h = map[r][0];
-				int seq = 0;
-				int seqX = X;
+				int h = map[r][0]; 			// 첫 요소의 높이
+				int seq = 0;				// (다음에 낮->높 일 경우를 대비) 다음 경사로 설치를 위해 확보 중인 길이 
+				int seqX = X;				// (이전에 높->낮 이었던 경우) 경사로 설치 중인 길이 (처음에는 경사로를 당장 설치할 필요 없으므로 X)
 				for (int c=0; c<N; c++) {
-					if (map[r][c] == h) {
-						if (seqX == X) {
-							seq++;
-						} else {
-							seqX++;
+					if (map[r][c] == h) {	// 같은 높이가 이어진다면
+						if (seqX == X) {		
+							seq++;				// 경사로를 다 설치했거나 설치할 필요가 없다면, seq++
+						} else {				
+							seqX++;				// 경사로가 아직 다 설치되지 않았다면 seqX++
 						}
 					} else if (map[r][c] == h+1) {
-						if (seq < X) continue outer;
-						h = map[r][c];
-						seq = 1;
-						seqX = X;
+						if (seq < X) continue outer;	// 낮->높 인데 경사로 설치를 위한 길이가 확보되지 않았다면 "불가능"
+						h++;							// 높이 갱신
+						seq = 1;						
+						seqX = X;						// 낮->높이므로 당장 설치할 필요없음
 					} else if (map[r][c] == h-1) {
-						if (seqX < X) continue outer;
-						h = map[r][c];
-						seq = 0;
-						seqX = 1;
+						if (seqX < X) continue outer;	// 높->낮 인데 경사로 설치가 완료되지 않았다면 "불가능"
+						h--;				
+						seq = 0;						
+						seqX = 1;				// 높->낮이므로 곧바로 경사로를 설치해야함. 지금 경사로 설치 완료 되어야 다음 경사로 길이(seq) 확보 가능
 					} else {
-						continue outer;
+						continue outer;			// 높이가 2개 이상차이나는경우 "불가능"
 					}
 				}
 				
-				if (seqX<X) continue;
+				if (seqX<X) continue;			// 마지막에 높->낮 이었는데 경사로 설치가 완료되지 않았다면 "불가능"
 //				System.out.println("r " + r);
-				sol++;
+				sol++;							// "불가능" 이 아니라면 카운트
 				
 			}
 			
@@ -75,12 +75,12 @@ public class Solution {
 						}
 					} else if (map[r][c] == h+1) {
 						if (seq < X) continue outer;
-						h = map[r][c];
+						h++;
 						seq = 1;
 						seqX = X;
 					} else if (map[r][c] == h-1) {
 						if (seqX < X) continue outer;
-						h = map[r][c];
+						h--;
 						seq = 0;
 						seqX = 1;
 					} else {
