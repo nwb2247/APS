6db1db264293868a8a0e79d2c8331d753dba7fa8
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-04-09 12:53:45 +0900

    [Gold IV] Title: 탈출, Time: 68 ms, Memory: 11776 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -15,14 +14,11 @@ public class Main {
 		int R = Integer.parseInt(st.nextToken());
 		int C = Integer.parseInt(st.nextToken());
 		
-		// 물과 비버 모두 pq로 관리
-		Queue<int[]> pqS = new ArrayDeque<>();
-		Queue<int[]> pqW = new ArrayDeque<>();
+		// 물과 고슴도치 모두 큐로 관리
+		Queue<int[]> qS = new ArrayDeque<>();
+		Queue<int[]> qW = new ArrayDeque<>();
 				
 		int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
-		
-		
-		// 물은 비어있거나 S인 경우 pq에 추가
 		
 		char[][] map = new char[R][];
 		for (int r=0; r<R; r++) {
@@ -30,31 +26,31 @@ public class Main {
 			for (int c=0; c<C; c++) {
 				switch(map[r][c]) {
 				case '*' : 
-					pqW.add(new int[] {r, c, 0});
+					qW.add(new int[] {r, c, 0});
 					break;
 				case 'S' : 
-					pqS.add(new int[] {r, c, 0});
+					qS.add(new int[] {r, c, 0});
 					break;
 				}
 			}
 		}
 		
-		
 		int t = 0;
 		outer : 
 		while (true) {
 			
-			while (!pqW.isEmpty() && pqW.peek()[2] == t) {
-				int[] w = pqW.poll();
+			// 물 채움
+			// 1초 뒤 물이 차는 곳에 고슴도치가 올 수 없으므로 물부터 채움
+			while (!qW.isEmpty() && qW.peek()[2] == t) {
+				int[] w = qW.poll();
 				int r = w[0];
 				int c = w[1];
-				
 				for (int d=0; d<4; d++) {
 					int nr = r+dir[d][0];
 					int nc = c+dir[d][1];
 					if (nr>=R || nc>=C || nr<0 || nc<0) continue;
-					if (map[nr][nc] == 'S' || map[nr][nc] == '.') {
-						pqW.add(new int[] {nr, nc, t+1});
+					if (map[nr][nc] == 'S' || map[nr][nc] == '.') { // 고슴도치가 지나간 자리이거나 빈 공간이면 채움
+						qW.add(new int[] {nr, nc, t+1});
 						map[nr][nc] = '*';
 					}
 				}
@@ -64,13 +60,14 @@ public class Main {
 //				System.out.println(Arrays.toString(map[i]));
 //			}
 			
-			if (pqS.isEmpty()) {
+			if (qS.isEmpty()) {	// 고슴도치가 있을 자리가 더이상 없으면 실패
 				System.out.println("KAKTUS");
 				break outer;
 			}
 			
-			while (!pqS.isEmpty() && pqS.peek()[2] == t) {
-				int[] s = pqS.poll();
+			// 고슴도치가 있을 수 있는 자리를 꺼내 1초뒤에 고슴도치가 있을 자리를 채움
+			while (!qS.isEmpty() && qS.peek()[2] == t) {
+				int[] s = qS.poll();
 				int r = s[0];
 				int c = s[1];
 				
@@ -78,12 +75,12 @@ public class Main {
 					int nr = r+dir[d][0];
 					int nc = c+dir[d][1];
 					if (nr>=R || nc>=C || nr<0 || nc<0) continue;
-					if (map[nr][nc] == 'D') {
+					if (map[nr][nc] == 'D') {			// 1초 뒤 비버굴에 도착한다면 리턴
 						System.out.println(t+1);
 						break outer;
 					}
 					if (map[nr][nc] == '.') {
-						pqS.add(new int[] {nr, nc, t+1});
+						qS.add(new int[] {nr, nc, t+1});
 						map[nr][nc] = 'S';
 					}
 				}
@@ -97,10 +94,7 @@ public class Main {
 			t++;
 			
 		}
-		
-		
-		
-		
+
 
 	}
 
