ad29ef3ecd243326ec1cbbce8db1cfee1311b0e7
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-07 21:48:03 +0900

    [Silver II] Title: 최소 힙, Time: 232 ms, Memory: 25104 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static final int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		for (int i=0; i<N; i++) {
+			int val = Integer.parseInt(br.readLine());
+			if (val == 0) {
+				if (pq.isEmpty()) {
+					sb.append(0).append("\n");
+				} else {
+					sb.append(pq.poll()).append("\n");
+				}
+			} else {
+				pq.offer(val);
+			}
+		}
+		
+		System.out.println(sb.toString());
+
+		
+	}
+}
