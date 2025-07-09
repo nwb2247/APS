1d24dd3197be6517f71a9bee9718b1cbe8948dee
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-09 22:35:22 +0900

    [Bronze IV] Title: 영수증, Time: 68 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int X = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			X -= Integer.parseInt(st.nextToken())*Integer.parseInt(st.nextToken());
+		}
+		if (X == 0) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+		
+		
+	}
+}
