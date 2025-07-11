998edba41836608a7406b341727d603b4f880784
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-11 22:55:50 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 64 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -6,67 +6,59 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
-	
-	// BJ1244
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		
-		int[] switches = new int[N+1];
-		for (int i=1; i<=N; i++) {
-			switches[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int M = Integer.parseInt(br.readLine());
-		
-		for (int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int num = Integer.parseInt(st.nextToken());
-			
-			if (s == 1) {
-				for (int j=num; j<=N; j += num) {
-					if (switches[j] == 1) {
-						switches[j] = 0;
-					} else {
-						switches[j] = 1;
-					}
-				}
-			} else { // s==2
-				for (int j=0; num+j<=N && num-j>=1; j++) {
-					if (switches[num+j] != switches[num-j]) {
-						break;
-					}
-					if (switches[num+j] == 1) {
-						switches[num+j] = 0;
-						switches[num-j] = 0;
-					} else {
-						switches[num+j] = 1;
-						switches[num-j] = 1;
-					}
-					
-				}
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i=1; i<=N; i++) {
-			sb.append(switches[i] + " ");
-			if (sb.length() == 40) {
-				System.out.println(sb);
-				sb = new StringBuilder();
-			}
-		}
-		
-		System.out.println(sb);
-	
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	StringBuilder sb = new StringBuilder();
+    	
+    	int N = Integer.parseInt(br.readLine());
+    	boolean[] arr = new boolean[N+1];
+    	st = new StringTokenizer(br.readLine());
+    	for (int i=1; i<=N; i++) {
+    		if (Integer.parseInt(st.nextToken()) == 1) {
+    			arr[i] = true;
+    		}	
+    	}
+    	
+    	int M = Integer.parseInt(br.readLine());
+    	for (int m=0; m<M; m++) {
+    		st = new StringTokenizer(br.readLine());
+    		int s = Integer.parseInt(st.nextToken());
+    		int i = Integer.parseInt(st.nextToken());
+    		if (s == 1) {
+    			for (int j=i; j<=N; j+=i) {
+    				arr[j] = !arr[j];
+    			}
+    		} else {
+    			int d = 0;
+    			while (i-d>=1 && i+d<=N && arr[i-d] == arr[i+d]) {
+    				d++;
+    			}
+    			d--;
+    			for (int j=i-d; j<=i+d; j++) {
+    				arr[j] = !arr[j];
+    			}
+    		}
+    	}
+    	
+    	for (int i=1; i<=N; i++) {
+    		if (arr[i]) {
+    			sb.append(1);
+    		} else {
+    			sb.append(0);
+    		}
+    		sb.append(" ");
+    		if (i%20 == 0) {
+    			sb.append("\n");
+    		}
+    	}
+    	
+    	System.out.println(sb.toString());
+    	
+    	
+    }
+    
 
-	}
-
+        
 }
