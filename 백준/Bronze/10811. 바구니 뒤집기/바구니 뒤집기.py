0fc8e228b391e387c7c4ba27f3c79cbd86f4a749
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-18 23:12:35 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -4,5 +4,5 @@ N, M = map(int, sys.stdin.readline().split())
 arr = list(range(0,N+1))
 for _ in range(M) :
     i, j = map(int, sys.stdin.readline().split())
-    arr[i:j+1] = arr[j:i-1:-1]
+    arr[i:j+1] = arr[i:j+1][::-1]
 print(*arr[1:])
