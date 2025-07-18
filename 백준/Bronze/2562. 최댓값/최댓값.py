db805d63de742a580db6e948242c71c91ffc476a
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-18 22:23:09 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,11 +1,17 @@
 import sys
 
-order = 0
-m = 0
-for i in range(1, 10) :
-     n = int(sys.stdin.readline())
-     if n > m :
-         m = n
-         order = i
+# order = 0
+# m = 0
+# for i in range(1, 10) :
+#      n = int(sys.stdin.readline())
+#      if n > m :
+#          m = n
+#          order = i
+# print(m)
+# print(order)
+nums = []
+for n in range(9) :
+    nums.append(int(sys.stdin.readline()))
+m = max(nums)
 print(m)
-print(order)
+print(nums.index(m)+1)
