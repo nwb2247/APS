4380d6289a395b4cf1503eeedf66bf052926e3aa
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-18 22:38:41 +0900

    [Bronze III] Title: 공 넣기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,12 +1,24 @@
 import sys
 
+# N, M = map(int, sys.stdin.readline().split())
+# arr = [0]*(N+1)
+# for _ in range(M) :
+#     i, j, k = map(int, sys.stdin.readline().split())
+#     for idx in range(i, j+1) :
+#         arr[idx] = k
+# output = []
+# for n in range(1,N+1) :
+#     output.append(str(arr[n]))
+# print(" ".join(output))
+
 N, M = map(int, sys.stdin.readline().split())
 arr = [0]*(N+1)
 for _ in range(M) :
     i, j, k = map(int, sys.stdin.readline().split())
     for idx in range(i, j+1) :
         arr[idx] = k
-output = []
-for n in range(1,N+1) :
-    output.append(str(arr[n]))
-print(" ".join(output))
+# output = []
+# for n in range(1,N+1) :
+#     output.append(str(arr[n]))
+# print(" ".join(output))
+print(*arr[1:])
