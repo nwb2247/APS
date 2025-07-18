85a8b655bbd9fdadbf6fbd2a2e9017b535f83cbd
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-18 21:54:10 +0900

    Update 개수 세기.py

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,8 +1,11 @@
 import sys
 
 N = int(sys.stdin.readline())
-cnt = [0]*201
+# cnt = [0]*201
 arr = list(map(int, sys.stdin.readline().split()))
-for i in range(N) :
-    cnt[arr[i]+100] += 1
-print(cnt[int(sys.stdin.readline())+100])
+# for i in range(N) :
+#     cnt[arr[i]+100] += 1
+V = int(sys.stdin.readline())
+# print(cnt[V]+100])
+
+print(arr.count(V))
