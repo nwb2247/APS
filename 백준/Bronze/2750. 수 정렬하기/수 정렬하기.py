d5e275ae7cb9c4e5444663231afcc47bf81adfe3
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-24 19:40:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 110708 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,79 +1,10 @@
+# swap을 이용한 정렬 (선택 정렬과 유사)
 N = int(input())
-input_lst = [int(input()) for _ in range(N)]
-
-def merge_sort(lst) :
-    if len(lst) == 1 :
-        return lst
-    # divide 부분
-    left = merge_sort(lst[:len(lst)//2])
-    right = merge_sort(lst[len(lst)//2:])
-    # conquer 부분
-    return merge(left, right)
-
-def merge(left, right) :
-    srt = [0]*(len(left)+len(right))
-    i, j, idx = 0, 0, 0
-    while i < len(left) and j < len(right) :
-        if left[i] <= right[j] :
-            srt[idx] = left[i]
-            idx += 1
-            i += 1
-        else :
-            srt[idx] = right[j]
-            idx += 1
-            j += 1
-    if i < len(left) :
-        # 개선 부분 (while문 대신 slicing으로 대입)
-        # list의 slicing이 좌변에 오면 해당 위치에 원소를 하나씩 대입하겠다는 뜻
-        # 우변에 오면 그 범위의 요소를 복사해 새 리스트를 만들겠다는 뜻
-        # 즉 같은 개수를 가진 list slicing을 대입하면 각 원소가 대체됨
-        # (단 갯수가 다르면 원본 배열의 길이가 늘어나거나 줄어들 수 있음)
-        srt[idx:] = left[i:]
-    else :
-        srt[idx:] = right[j:]
-    return srt
-print(*merge_sort(input_lst), sep="\n")
-
-
-"""
-1차 시도
-N = int(input())
-input_lst = [int(input()) for _ in range(N)]
-
-def merge_sort(lst) :
-    if len(lst) == 1 :
-        return lst
-    # divide 부분
-    left = merge_sort(lst[:len(lst)//2])
-    right = merge_sort(lst[len(lst)//2:])
-    # conquer 부분
-    return merge(left, right)
-
-def merge(left, right) :
-    srt = [0]*(len(left)+len(right))
-    i, j, idx = 0, 0, 0
-    while i<len(left) and j<len(right) :
-        if left[i] <= right[j] :
-            srt[idx] = left[i]
-            idx += 1
-            i += 1
-        else :
-            srt[idx] = right[j]
-            idx += 1
-            j += 1
-    if i < len(left) :
-        while i < len(left) :
-            srt[idx] = left[i]
-            idx += 1
-            i += 1
-    else :
-        while j < len(right) :
-            srt[idx] = right[j]
-            idx += 1
-            j += 1
-    return srt
-print(*merge_sort(input_lst), sep="\n")
-"""
-
-
-
+lst = [int(input()) for _ in range(N)]
+for i in range(N-1) : # N-2번째까지 정렬을 마치면 N-1(마지막)번째는 자동으로 가장 큰 값이 오게 됨
+    # i : i번째로 작은 값을 찾아 넣을 것
+    # i+1부터 끝까지 lst[i]와 비교하여 swap하고, 가장 작은 값이 i번째에 오도록 함
+    for j in range(i+1, N) :
+        if lst[j] < lst[i] :
+            lst[i], lst[j] = lst[j], lst[i]
+print(*lst)
