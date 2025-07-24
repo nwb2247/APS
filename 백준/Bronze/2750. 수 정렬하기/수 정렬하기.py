bccebaa0ddcc42830b651577aa9040a09caf62b8
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-24 17:22:24 +0900

    [Bronze II] Title: 수 정렬하기, Time: 120 ms, Memory: 110916 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,9 +1,37 @@
-import sys
+N = int(input())
+input_lst = [int(input()) for _ in range(N)]
 
-N = int(sys.stdin.readline())
-arr = [0]*N
-for i in range(N) :
-    arr[i] = int(sys.stdin.readline())
-arr.sort()
-for i in range(N) :
-    print(arr[i])
+def merge_sort(lst) :
+    if len(lst) == 1 :
+        return lst
+    # divide 부분
+    left = merge_sort(lst[:len(lst)//2])
+    right = merge_sort(lst[len(lst)//2:])
+    # conquer 부분
+    return merge(left, right)
+
+def merge(left, right) :
+    srt = [0]*(len(left)+len(right))
+    i, j, idx = 0, 0, 0
+    while i<len(left) and j<len(right) :
+        if left[i] <= right[j] :
+            srt[idx] = left[i]
+            idx += 1
+            i += 1
+        else :
+            srt[idx] = right[j]
+            idx += 1
+            j += 1
+    if i < len(left) :
+        while i < len(left) :
+            srt[idx] = left[i]
+            idx += 1
+            i += 1
+    else :
+        while j < len(right) :
+            srt[idx] = right[j]
+            idx += 1
+            j += 1
+    return srt
+print(*merge_sort(input_lst), sep="\n")
+
