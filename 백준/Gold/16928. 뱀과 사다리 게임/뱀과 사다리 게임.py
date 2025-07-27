ea37727c096f61ed8ed6eed8cbfe76ede1071561
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-27 21:01:32 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 124 ms, Memory: 110068 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -8,13 +8,18 @@ for _ in range(N+M):
     s, e = map(int, input().split())
     lst[s] = e
 
+# visited 사용하여 방문했던 정점 다시 큐에 넣지 않아야 메모리 초과 나지 않음
+visited = [False]*101
+
 q = deque()
 # 위치, 굴린 횟수
 q.append((1, 0))
 ans = 0
-visited = [False]*101
+
 while len(q) > 0:
     cur, cnt = q.popleft()
+    if visited[cur]:
+        continue
     visited[cur] = True
     # 뱀, 사다리 적용
     dest = lst[cur] if lst[cur] != 0 else cur
