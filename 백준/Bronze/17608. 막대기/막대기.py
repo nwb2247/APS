edd6ae309b293583daaa1d7f43611d71ca5afe59
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-28 15:51:23 +0900

    Update 막대기.py

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -1,10 +1,15 @@
+"""
+스택으로 풀어보기
+top of stk 이 무얼 의미하는지 생각하자
+왼쪽부터 하나씩 추가할 때, 추가하는 것이 앞의 것들을 가리는지 생각해보자.
+가려지는 것이 없을 때까지 pop
+"""
+
 N = int(input())
-lst = [int(input()) for _ in range(N)]
-cnt = 0 
-mx = 0 # 현재까지의 최대값 (최대값보다 작거나 같은 높이는 보이지 않으므로)
-# 뒤에서부터 순회
-for num in lst[::-1]:
-    if num > mx: # 더 높은 것이 발견되면 cnt 추가하고 mx도 갱신
-        cnt += 1
-        mx = num
-print(cnt)
+stk = []
+for _ in range(N):
+    new = int(input())
+    while len(stk) != 0 and stk[-1] <= new: # 가려지는 것이 없을 때까지 pop
+        stk.pop()
+    stk.append(new)
+print(len(stk))
