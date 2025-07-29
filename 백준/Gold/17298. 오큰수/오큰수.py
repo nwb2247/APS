986465cf44cc324d448f2fbe2630863b915dd7a4
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-29 10:42:42 +0900

    [Gold IV] Title: 오큰수, Time: 484 ms, Memory: 251940 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,16 +1,17 @@
-"""
-스택으로 풀어보기 (시간 복잡도 O(N)?)
+def solve():
+    N = int(input())
+    lst = list(map(int, input().split()))
+    stk = []
+    ans = [-1]*N
+    for i in range(N-1,-1,-1):
+        # 오큰수가 나올 때까지 pop
+        while len(stk) != 0 and stk[-1] <= lst[i]:
+            stk.pop()
+        # stk이 비어있지 않다면 (즉, 오큰수가 존재한다면)
+        if len(stk) != 0:
+            ans[i] = stk[-1]
+        # 이번엔 자기 자신을 stk에 넣음
+        stk.append(lst[i])
+    print(*ans)
 
-오큰수 찾자마다 바로 pop하므로
-각 인덱스에 대해 push pop이 각한번씩만 이루어지므로 O(N)
-"""
-N = int(input())
-lst = list(map(int, input().split()))
-stk = []
-ans = [-1]*N    # 발견하지 못한 경우 -1출력하므로 -1로 초기화
-for i in range(N):
-    while len(stk) != 0 and lst[stk[-1]] < lst[i]: # lst[i]가 오큰수가 되는 모든 값을  pop
-        ans[stk.pop()] = lst[i] # ans에 기록
-    stk.append(i) # i도 stk에 push
-print(*ans)
-
+solve()
