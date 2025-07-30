6b91c03051bfa1839f4cd27d0c333f573a8769a6
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-07-30 19:45:56 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,14 +1,15 @@
-import sys
+# 7:42~7:45
+# 구현
 
-paper = [[False for j in range(100)] for i in range(100)]
-
-N = int(sys.stdin.readline())
-
-for _ in range(N) :
-    r, c = map(int, sys.stdin.readline().split())
-    for i in range(r,r+10) :
-        for j in range(c,c+10) :
-            paper[i][j] = True
-
-print(sum(map(sum, paper)))
-
+arr = [[False]*100 for _ in range(100)]
+N = int(input())
+for _ in range(N):
+    sr, sc = map(int, input().split())
+    for r in range(sr, sr+10):
+        for c in range(sc, sc+10):
+            arr[r][c] = True
+cnt = 0
+for r in range(100):
+    for c in range(100):
+        if arr[r][c] : cnt += 1
+print(cnt)
