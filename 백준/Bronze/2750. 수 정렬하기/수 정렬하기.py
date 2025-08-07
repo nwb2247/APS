800bb6f2eef876341cffe03d96df841bb0b2044d
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-07 11:58:40 +0900

    Update 수 정렬하기.py
    병합, 퀵소트 (배열 인덱스), 파이썬용 퀵소트 (리스트)

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,36 +1,68 @@
-# 배열 인덱스를 이용한 퀵소트
-def qsort(start, end): # 시작인덱스, 마지막인덱스
-    if end-start <= 0: # 종료 조건 비어있거나 하나만 있다면 -> 이미 정렬 (비어있는 경우는 end<start)
-        return
+def merge_sort(l):
+    if len(l) <= 1:
+        return l
 
-    pivot = lst[start]
-    left = start+1
-    right = end
-    while left<right: # 다른 동안 진행, 즉 left == right 되면 종료
-        if lst[left] <= pivot:      # (D) 같은 수가 올 수 있으므로
-            left += 1
-        elif lst[right] >= pivot:   # (D) 같은 수가 올 수 있으므로
-            right -= 1
-        else :
-        # lst[left]는 pivot보다 크고, lst[right]는 pivot 보다 작은 상태라 바꿀 준비가 되었다면
-            lst[left], lst[right] = lst[right], lst[left]
+    # 반으로 나누고 병합 정렬
+    left = merge_sort(l[:len(l)//2])
+    right = merge_sort(l[len(l)//2:])
 
-    # lst[left]가 pivot보다 크다면, lst[left-1]가 pivot보다 작거나 같은 상태이므로 pivot과 자리를 바꿀 수 있음
-    if lst[left] > pivot:
-        lst[left-1], lst[start] = lst[start], lst[left-1]
-        qsort(start, left-2) # pivot이 left-1로 이동했으므로 왼쪽은 start~left-2 오른쪽은 left~end
-        qsort(left, end)
-    # lst[left]가 pivot보다 작거나 같다면, 바로 pivot과 자리를 바꿀 수 있음
-    else:
-        lst[left], lst[start] = lst[start], lst[left]
-        qsort(start, left-1) # pivot이 left로 이동했으므로 왼쪽은 start~left-1 오른쪽은 left+!~end
-        qsort(left+1, end)
-
+    # 정렬된 양쪽의 결과를 합쳐줌
+    li, ri = 0, 0   # 왼쪽 인덱스 오른쪽 인덱스 초기화
+    ret = [0]*len(l)
+    for i in range(len(l)):
+        if li >= len(left):     # 왼쪽 리스트 다썼다면
+            ret[i] = right[ri]
+            ri += 1
+        elif ri >= len(right):  # 오른쪽 리스트 다썼다면
+            ret[i] = left[li]
+            li += 1
+        else:                   # 둘다 남아있다면 작은 것을
+            if left[li] < right[ri]:
+                ret[i] = left[li]
+                li += 1
+            else:
+                ret[i] = right[ri]
+                ri += 1
+    return ret
 
 N = int(input())
 lst = [int(input()) for _ in range(N)]
-qsort(0, N-1)
-print(*lst, sep="\n")
+
+print(*merge_sort(lst), sep="\n")
+
+# # 배열 인덱스를 이용한 퀵소트
+# def qsort(start, end): # 시작인덱스, 마지막인덱스
+#     if end-start <= 0: # 종료 조건 비어있거나 하나만 있다면 -> 이미 정렬 (비어있는 경우는 end<start)
+#         return
+#
+#     pivot = lst[start]
+#     left = start+1
+#     right = end
+#     while left<right: # 다른 동안 진행, 즉 left == right 되면 종료
+#         if lst[left] <= pivot:      # (D) 같은 수가 올 수 있으므로
+#             left += 1
+#         elif lst[right] >= pivot:   # (D) 같은 수가 올 수 있으므로
+#             right -= 1
+#         else :
+#         # lst[left]는 pivot보다 크고, lst[right]는 pivot 보다 작은 상태라 바꿀 준비가 되었다면
+#             lst[left], lst[right] = lst[right], lst[left]
+#
+#     # lst[left]가 pivot보다 크다면, lst[left-1]가 pivot보다 작거나 같은 상태이므로 pivot과 자리를 바꿀 수 있음
+#     if lst[left] > pivot:
+#         lst[left-1], lst[start] = lst[start], lst[left-1]
+#         qsort(start, left-2) # pivot이 left-1로 이동했으므로 왼쪽은 start~left-2 오른쪽은 left~end
+#         qsort(left, end)
+#     # lst[left]가 pivot보다 작거나 같다면, 바로 pivot과 자리를 바꿀 수 있음
+#     else:
+#         lst[left], lst[start] = lst[start], lst[left]
+#         qsort(start, left-1) # pivot이 left로 이동했으므로 왼쪽은 start~left-1 오른쪽은 left+!~end
+#         qsort(left+1, end)
+#
+#
+# N = int(input())
+# lst = [int(input()) for _ in range(N)]
+# qsort(0, N-1)
+# print(*lst, sep="\n")
 
 
 
@@ -53,3 +85,5 @@ print(*lst, sep="\n")
 #     return qsort(left) + [pivot] + qsort(right)
 #
 # print(*qsort(lst), sep="\n")
+
+
