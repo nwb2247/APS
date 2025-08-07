695ede335a08c21086e9c713e6791d42a30cb52c
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-07 11:18:38 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 4일차 - 퀵 정렬, Time: 1,048 ms, Memory: 177,280 KB -BaekjoonHub

---
+++ SWEA/D3/5205. ［파이썬 S／W 문제해결 구현］ 4일차 － 퀵 정렬/［파이썬 S／W 문제해결 구현］ 4일차 － 퀵 정렬.py
@@ -0,0 +1,51 @@
+# 배열 인덱스를 이용한 퀵소트
+def qsort(start, end): # 시작인덱스, 마지막인덱스
+    if end-start <= 0: # 종료 조건 비어있거나 하나만 있다면 -> 이미 정렬 (비어있는 경우는 end<start)
+        return
+
+    pivot = lst[start]
+    left = start+1
+    right = end
+    while left<right: # 다른 동안 진행, 즉 left == right 되면 종료
+        if lst[left] <= pivot:      # (D) 같은 수가 올 수 있으므로
+            left += 1
+        elif lst[right] >= pivot:	# (D) 같은 수가 올 수 있으므로
+            right -= 1
+        else :
+            lst[left], lst[right] = lst[right], lst[left]
+    if lst[left] > pivot:
+        lst[left-1], lst[start] = lst[start], lst[left-1]
+        qsort(start, left-2)
+        qsort(left, end)
+    else:
+        lst[left], lst[start] = lst[start], lst[left]
+        qsort(start, left-1)
+        qsort(left+1, end)
+
+TC = int(input())
+for tc in range(1, TC+1):
+    N = int(input())
+    lst = list(map(int, input().split()))
+    qsort(0, N - 1)
+    print(f"#{tc} {lst[N//2]}")
+
+
+# # 리스트를 이용한 퀵소트
+# def qsort(l):
+#     if len(l) <= 1: # 종료 조건 비어있거나 하나만 있다면 -> 이미 정렬
+#          return l
+#
+#     pivot = l.pop() # 맨 마지막 원소 pop하여 피벗으로 (마지막을 pop하는게 제일 빠르므로)
+#
+#     left = [] # 빈리스트 생성, 피벗보다 작다면 왼쪽, 크다면 오른쪽에 append
+#     right = []
+#     for e in l:
+#         if e<pivot:
+#             left.append(e)
+#         else:
+#             right.append(e)
+#
+#     #left, right에 대해서도 퀵소트 시킴
+#     return qsort(left) + [pivot] + qsort(right)
+#
+# print(*qsort(lst), sep="\n")
