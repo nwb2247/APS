557d25baf42ed4ac04bd664c1b079dd8509c6b4d
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-07 15:49:48 +0900

    Update 하노이 탑 이동 순서.py

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,9 +1,9 @@
 N = int(input())
 
 # (시작지, 도착지) -> 경유지
-idle = {(1, 3):2, (3, 1):2,
-      (2, 3):1, (3, 2):1,
-      (1, 2):3, (2, 1):3}
+idle = {(1, 3): 2, (3, 1): 2,
+        (2, 3): 1, (3, 2): 1,
+        (1, 2): 3, (2, 1): 3}
 
 """
 N칸짜리를 s에서 d으로 옮기려면 
@@ -11,20 +11,20 @@ N칸짜리를 s에서 d으로 옮기려면
 맨아래 가장 큰 파트를 s에서 d로 옮긴후
 다시 N-1짜리를 idle에서 d로 옮겨야함
 """
-def recur(N, s, d): # N: 몇칸짜리인지, s시작, d도착
+
+
+def recur(N, s, d):  # N: 몇칸짜리인지, s시작, d도착
     if N == 1:
         ans.append((s, d))
-        return 1
-    cnt = 0
-    cnt += recur(N - 1, s, idle[(s, d)])
-    cnt += 1
-    ans.append((s, d))
-    cnt += recur(N - 1, idle[(s, d)], d)
+        return
+    recur(N - 1, s, idle[(s, d)])  # 일단 위 N-1칸을 s에서 남는 곳(idle)로 옮기고                     # 맨아래 가장 큰 파트를 s에서 d로 옮긴후
+    recur(1, s, d)
+    recur(N - 1, idle[(s, d)], d)  # 다시 N-1짜리를 idle에서 d로 옮겨야함
 
-    return cnt
 
+cnt = 0
 ans = []
-print(recur(N, 1, 3))
+recur(N, 1, 3)
+print(len(ans))
 for move in ans:
     print(*move)
-
