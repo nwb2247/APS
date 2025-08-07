760bb4cfea4ea7d1cf0f7dacbba30ba38e92450c
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-07 09:19:27 +0900

    [Silver IV] Title: 카드 놓기, Time: 96 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -10,16 +10,19 @@ N<=10 장 카드에서 K장 선택하고 나란히 정수 만들기
 (10-2)!
 
 """
-def dfs(depth, st): # depth : 직전까지 넣은 카드 수, st 현재까지 추가된 문자열
+def dfs(depth): # depth : 직전까지 넣은 카드 수, st 현재까지 추가된 문자열
 
     if depth == K:      # 종료 조건 (K개를 모두 골랐다면)
-        ansset.add(st)
+        ansset.add("".join(ans))
+        return # (D) 종료 조건에서 return 안해줬음...
 
     for i in range(N):  # 아직 넣지 않은 카드 고르기
         if visited[i] == 0:
             visited[i] = 1
-            dfs(depth+1, st+lst[i])
+            ans.append(lst[i])
+            dfs(depth+1)
             visited[i] = 0
+            ans.pop()
 
 N = int(input())
 K = int(input())
@@ -29,6 +32,7 @@ lst = [input() for _ in range(N)]
 # print(lst)
 N = len(lst)
 visited = [0]*N
+ans = []
 ansset = set()
-dfs(0, "")
+dfs(0)
 print(len(ansset))
