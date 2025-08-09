c15e0f008fe202153d8418c5afbd58da4d1f45bd
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-10 00:02:39 +0900

    Update 트리의 순회.py

--- 백준/Gold/2263. 트리의 순회/트리의 순회.py
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.py
@@ -6,9 +6,15 @@ inoder, postorder 주어지면
  preorder로 출력
 
 [접근]
-[--left--][p][--right--]
-[--left--][--right--][p]
-posto에서 마지막을 p로 잡고
+ino =>   [--left--][p][--right--]
+posto => [--left--][--right--][p]
+posto에서 마지막을 p로 잡고 ino에서 p의 인덱스를 찾아 offset(p의 인덱스 - left의 시작)을 잡음
+
+ino의 left =>    ino_s                   ~ ino_s + offset - 1
+ino의 right=>    ino_s + offset + 1      ~ ino_e
+posto의 left =>  posto_s                 ~ posto_s + offset - 1
+posto의 right => posto_s + offset        ~ posto_e - 1
+
 =>
 [p][--left--][--right--]
 
@@ -23,21 +29,20 @@ def preo(ino_s, ino_e, posto_s, posto_e):
     if ino_s > ino_e:
         return
 
-    p = posto[posto_e]
-    ino_pi = ino_idx[p]
-    # 주의 : [ino_s:ino_e+1]에서만 찾지 않으면 시간 터짐
+    p = posto[posto_e]      # posto 범위의 마지막 : 부모 노드 p
+    ino_pi = ino_idx[p]     # ino에서 부모 노드 p의 인덱스 찾음
 
-    offset = (ino_pi - 1) - ino_s
-    ans.append(p)
-    preo(ino_s, ino_s + offset, posto_s, posto_s + offset)
-    preo(ino_s + offset + 2, ino_e, posto_s + offset + 1, posto_e - 1)
+    offset = ino_pi - ino_s
+    ans.append(p)           # preorder해야하므로 부모부터
+    preo(ino_s, ino_s + offset - 1, posto_s, posto_s + offset - 1)  # 왼쪽
+    preo(ino_s + offset + 1, ino_e, posto_s + offset, posto_e - 1)  # 오른쪽
 
 
 ans = []
 N = int(input())
 ino = list(map(int, input().split()))
 posto = list(map(int, input().split()))
-ino_idx = [0]*(N+1)
+ino_idx = [0]*(N+1)                 # (D) 전처리로 인덱스 빠르게 찾기
 for i, num in enumerate(ino):
     ino_idx[num] = i
 
