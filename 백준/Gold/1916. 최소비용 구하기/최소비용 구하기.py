e865fc59288ae372a0ee866f80e86490342432ba
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-13 11:43:35 +0900

    Update 최소비용 구하기.py

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -3,7 +3,7 @@
 조건 : 음수 가중치 없음 (0이상)
 
 일종의 그리디
-방문하지 않은 정점 중, 시작점과의 거리가 최소거리인 정점을 선택한다.
+최소거리가 확정되지 않은 정점 중, 시작점과의 거리가 최소거리인 정점을 선택한다.
 
 직관적인 이해:
     음수 가중치가 없으니까 "먼저 확정한 정점"을 더 짧게 만들수 없다.
@@ -21,36 +21,61 @@ for _ in range(M):
 
 S, E = map(int, input().split())
 
-visited = [0]*(N+1)             # 방문한 정점의 거리 (최단거리가 확정된...)
+confirmed = [0]*(N+1)           # 최단거리 확정 여부 (방문과는 다르므로 혼동피하게 위해 visited 미사용..)
 dist = [float('inf')]*(N+1)     # S에서 다른 모든 정점과의 거리
 
 # 시작점 자기 자신과의 거리는 0
 dist[S] = 0
-# print(visited)
-# print(dist)
-# print()
 
-# 다익스트라 시작
-while visited[E] == 0:    # E의 최단거리가 확정될때까지 계속 수행
+# 우선순위를 이용한 다익스트라
+from heapq import heappop, heappush
+pq = []
+heappush(pq, (0, S))
 
-    # [1] 최단거리 확정 되지 않은 정점 중 가장 S와의 거리가 가장 짧은 정점 cur 선택  (dist[cur]은 최단거리 확정)
-    mn = float('inf')
-    cur = None
-    for i in range(1, N+1):
-        if visited[i] == 0 and dist[i] < mn:   # 아직 최단거리 확정되지 않은 정점 중 가장 짧은 정점
-            cur = i
-            mn = dist[i]
+while pq:
 
-    # [2] dist[cur]은 최단거리 확정되었으므로 방문처리
-    visited[cur] = 1
+    curdist, cur = heappop(pq)
 
-    # [3] 선택한 것과 인접한 정점들에 대해서 기존보다 S에 더 짧게 도달할 수 있는 것이 있다면 dist에 갱신해줌
-    for w, adj_node in adj[cur]:
-        dist[adj_node] = min(dist[adj_node], dist[cur] + w)
+    if confirmed[cur] == 1:
+        continue
+        # 이미 확정되었다면 건너뜀
+        # (pq를 사용하면 dist의 갱신이 이뤄질때마다 우선순위큐에 들어가므로, 여러 개가 들어갈 수 있고, 확정 후에도 남아 있을 수 있음)
 
-    # print(cur, visited)
-    # print(dist)
-    # print()
+    # 지금 꺼내진 것이 S와의 거리가 가장 짧은 것이므로 최단거리임이 확정됨
+    confirmed[cur] = 1
+
+    if cur == E: # 도착정점을 찾았다면 break
+        break
+
+    for w, nxt in adj[cur]:
+        if confirmed[nxt] == 0 and dist[cur] + w < dist[nxt]:  # (D) and 써야하는데 or 사용;;
+        # 아직 확정되지 않았고, cur을 경유함으로써 더 짧은 거리가 가능해졌다면 갱신하고, pq에 넣어줌
+            dist[nxt] = dist[cur] + w
+            heappush(pq, (dist[nxt], nxt))
 
 print(dist[E])
+
+
+
+# # 루프를 이용한 고전적 방식의 다익스트라
+# # 다익스트라 시작
+# while confirmed[E] == 0:    # E의 최단거리가 확정될때까지 계속 수행
+#
+#     # [1] 최단거리 확정 되지 않은 정점 중 가장 S와의 거리가 가장 짧은 정점 cur 선택  (dist[cur]은 최단거리 확정)
+#     mn = float('inf')
+#     cur = None
+#     for i in range(1, N+1):
+#         if confirmed[i] == 0 and dist[i] < mn:   # 아직 최단거리 확정되지 않은 정점 중 가장 짧은 정점
+#             cur = i
+#             mn = dist[i]
+#
+#     # [2] dist[cur]은 최단거리 확정되었으므로 확정
+#     confirmed[cur] = 1
+#
+#     # [3] 선택한 것과 인접한 정점들에 대해서 기존보다 S에 더 짧게 도달할 수 있는 것이 있다면 dist에 갱신해줌
+#     for w, nxt in adj[cur]:
+#         dist[nxt] = min(dist[nxt], dist[cur] + w)
+#
+# # print(dist)
+# print(dist[E])
 
