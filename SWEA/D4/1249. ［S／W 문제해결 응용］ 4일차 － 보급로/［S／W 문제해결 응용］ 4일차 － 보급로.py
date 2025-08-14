1ede285611fd3176d2188fd4c4be09d03274ede8
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-14 10:25:29 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 166 ms, Memory: 62,592 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -1,7 +1,9 @@
 """
-다익스트라
+BFS 응용 -> 최소값 갱신 시 append 하는 BFS
+=> 단 갱신이 더 이뤄질 수 있으므로 도착지 만났다고 해서 멈추면 안됨
 """
-from heapq import heappop, heappush
+
+from collections import deque
 
 ds = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
@@ -10,32 +12,63 @@ for tc in range(1, TC+1):
     N = int(input())
     arr = [list(map(int, input())) for _ in range(N)]
 
-    pq = []
-    heappush(pq, (0, 0, 0)) # 거리, r, c
+    visited = [[N*N*10]*N for _ in range(N)] # 최소 갱신시 append 위해 가능한 최대값을 넣어줌
+    q = deque()
 
-    dist = [[N*N*10]*N for _ in range(N)]
-    dist[0][0] = 0
+    visited[0][0] = 0
+    q.append((0, 0))
+    
+    while q:
+        cr, cc = q.popleft()
 
-    while pq:
-        curd, cr, cc = heappop(pq)
-
-        if curd > dist[cr][cc]:
-            continue
-
-        if (cr, cc) == (N-1, N-1):
-            break
-
-        for dr, dc in ds:
+        for dr, dc, in ds:
             nr, nc = cr+dr, cc+dc
-            if 0 <= nr < N and 0 <= nc < N and curd + arr[nr][nc] < dist[nr][nc]:
-                dist[nr][nc] = curd + arr[nr][nc]
-                heappush(pq, (dist[nr][nc], nr, nc))
+            if 0<=nr<N and 0<=nc<N:
+                if visited[cr][cc] + arr[nr][nc] < visited[nr][nc]:
+                    visited[nr][nc] = visited[cr][cc] + arr[nr][nc] # 갱신
+                    if arr[nr][nc] == 0:    # 0-1 BFS : 최소값이 빠르게 갱신되는걸(arr[nr][nc] == 0) 먼저 pop되게 해서 전체적인 갱신 수가 줄어들도록 한다.
+                        q.appendleft((nr, nc))
+                    else:
+                        q.append((nr, nc))
 
-    print(f"#{tc} {dist[N-1][N-1]}")
+    print(f"#{tc} {visited[N-1][N-1]}")
 
-
-
-
+# """
+# 다익스트라
+# """
+# from heapq import heappop, heappush
+#
+# ds = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+#
+# TC = int(input())
+# for tc in range(1, TC+1):
+#     N = int(input())
+#     arr = [list(map(int, input())) for _ in range(N)]
+#
+#     pq = []
+#     heappush(pq, (0, 0, 0)) # 거리, r, c
+#
+#     dist = [[N*N*10]*N for _ in range(N)]
+#     dist[0][0] = 0
+#
+#     while pq:
+#         curd, cr, cc = heappop(pq)
+#
+#         if curd > dist[cr][cc]:
+#             continue
+#
+#         if (cr, cc) == (N-1, N-1):
+#             break
+#
+#         for dr, dc in ds:
+#             nr, nc = cr+dr, cc+dc
+#             if 0 <= nr < N and 0 <= nc < N and curd + arr[nr][nc] < dist[nr][nc]:
+#                 dist[nr][nc] = curd + arr[nr][nc]
+#                 heappush(pq, (dist[nr][nc], nr, nc))
+#
+#     print(f"#{tc} {dist[N-1][N-1]}")
+#
+#
 
 
 # """
