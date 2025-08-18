c5a7340266f4e8cc728283999938745da3f050af
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-18 20:20:17 +0900

    [Gold V] Title: 매직 스타, Time: 1028 ms, Memory: 113088 KB -BaekjoonHub

--- 백준/Gold/3967. 매직 스타/매직 스타.py
+++ 백준/Gold/3967. 매직 스타/매직 스타.py
@@ -2,7 +2,6 @@
 항상 정답이 존재하는 경우만 주어짐
 백트래킹
 사전 순으로 가장 앞서는 방법만 출력, 즉 처음 발견하면 모두 종료
-가지치기
 """
 
 ARR = [list(input()) for _ in range(5)]
@@ -13,25 +12,20 @@ idxs = [(0, 4),
         (4, 4)]
 
 lst = [0]*12
+empty = []
+use = [0]*13
 for i, (r, c) in enumerate(idxs):
     if ARR[r][c] == "x":
         lst[i] = 0
-    else:
-        lst[i] = ord(ARR[r][c]) - ord("A") + 1
-
-empty = []
-use = [0]*13
-for i in range(12):
-    if lst[i] == 0:
         empty.append(i)
     else:
+        lst[i] = ord(ARR[r][c]) - ord("A") + 1
         use[lst[i]] = 1
+
 no_use = []
-for i in range(1, 13):  # 자동으로 정렬됨.
+for i in range(1, 13):  # 자동으로 정렬됨. (사전 순으로 가장 앞서는 것 출력 가능)
     if use[i] == 0:
         no_use.append(i)
-
-# print(empty, no_use)
 
 groups = [[0, 2, 5, 7],
           [1, 2, 3, 4],
@@ -40,38 +34,28 @@ groups = [[0, 2, 5, 7],
           [7, 8, 9, 10],
           [0, 3, 6, 10]]
 
-where = [[] for _ in range(12)]
-for i in range(12):
-    for j, g in enumerate(groups):
-        if i in g:
-            where[i].append(j)
-
 L = len(empty)
-ans = [0]*L
+ans = []
 found = False
 visited = [0]*L # no_use[i]를 썼는지 안썼는지
-final = []
 
 def check():
-    global found, final
+    global found, ans
     if found:
-        False
+        return
 
-    tmp = lst[:]
-    for i in range(L):
-        tmp[empty[i]] = ans[i]
     for g in groups:
         sm = 0
         for i in g:
-            sm += tmp[i]
+            sm += lst[i]
         if sm != 26:
-            return False
-    final = tmp
+            return
+
     found = True
-    return True
+    ans = lst[:]
 
 def backtrack(depth):
-    global ans, found
+    global found
 
     if found:
         return
@@ -83,12 +67,14 @@ def backtrack(depth):
     for i in range(L):
         if visited[i] == 0:
             visited[i] = 1
-            ans[depth] = no_use[i]
+            lst[empty[depth]] = no_use[i]
             backtrack(depth + 1)
+            if found:
+                return
             visited[i] = 0
 
 backtrack(0)
-output = [chr(num + ord("A") - 1) for num in final]
+output = [chr(num + ord("A") - 1) for num in ans]
 for i, (r, c) in enumerate(idxs):
     ARR[r][c] = output[i]
 for l in ARR:
