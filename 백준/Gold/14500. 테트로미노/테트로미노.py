60c9d8bbfa143a9915bce26e5b3e530d8e28a79e
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-19 21:39:43 +0900

    [Gold IV] Title: 테트로미노, Time: 388 ms, Memory: 112516 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,5 +1,7 @@
 """
 회전 4 * 대칭 3 가지 * 나의 위치 4가지 * 테트로노미노 모양 5 => 160가지
+(D) 생각해보면 나의 위치 4는 곱할 필요가 없음
+why? 어차피 다른 점에서 시작했을떄 내가 포함되는 경우가 있기때문
 N*M = 4*500
 완탐 시간 OK
 
@@ -7,18 +9,17 @@ N*M = 4*500
 각 모양에서 한점을 잡았을때 나머지의 상대위치 변환 시켜주는 함수
 4개가 전부 범위에서 벗어나는지 아닌지 확인
 """
-tetros = [[(0, 0), (0, 1), (0, 2), (0, 3)],
-          [(0, 0), (1, 0), (2, 0), (2, 1)],
-          [(0, 0), (1, 0), (1, 1), (2, 1)],
-          [(0, 0), (0, 1), (0, 2), (1, 1)],
-          [(0, 0), (0, 1), (1, 0), (1, 1)]]
-# tetros = [[(0, 0), (1, 0), (2, 0), (2, 1)]]
+tetros = [((0, 0), (0, 1), (0, 2), (0, 3)), # 중복 처리 위해 tuple로 받음 (이중 튜플도 set에 넣을 수 있음)
+          ((0, 0), (1, 0), (2, 0), (2, 1)),
+          ((0, 0), (1, 0), (1, 1), (2, 1)),
+          ((0, 0), (0, 1), (0, 2), (1, 1)),
+          ((0, 0), (0, 1), (1, 0), (1, 1))]
 # 5개
 
-# 회전
+# 회전 (180도)
 new = []
 for t in tetros:
-    tmp = [(c, r) for r, c in t]
+    tmp = tuple([(c, r) for r, c in t])
     new.append(tmp)
 tetros += new
 # 10개 (180도)
@@ -26,24 +27,30 @@ tetros += new
 # 대칭
 new = []
 for t in tetros:
-    tmp = [(-r, c) for r, c in t]
+    tmp = tuple([(-r, c) for r, c in t])
     new.append(tmp)
-    tmp = [(r, -c) for r, c in t]
+    tmp = tuple([(r, -c) for r, c in t])
     new.append(tmp)
-    tmp = [(-r, -c) for r, c in t]
+    tmp = tuple([(-r, -c) for r, c in t])
     new.append(tmp)
 tetros += new
 # 40개
+sset = set(tetros)
+# len(sset) # 36개
+tetros = list(sset)
+# for t in tetros:
+#     print(t)
 
-new = []
-for t in tetros:
-    for br, bc in t:
-        tmp = []
-        for r, c in t:
-            tmp.append((r-br, c-bc))
-        new.append(tmp)
-tetros = new # (new에 tetro가 포함되어있음)
-# 160개
+
+# new = []                           (D) 어차피 다른 점 시작에서 내가 포함될 수 있으므로 이부분은 굳이 X
+# for t in tetros:
+#     for br, bc in t:
+#         tmp = []
+#         for r, c in t:
+#             tmp.append((r-br, c-bc))
+#         new.append(tuple(tmp))
+# tetros = new # (new에 tetro가 포함되어있음)
+# # 160개
 
 N, M = map(int, input().split())
 ARR = [list(map(int, input().split())) for _ in range(N)]
