fd9a89dd30cf0f5684deceb975bd45095f91c398
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-19 17:42:22 +0900

    [Gold V] Title: 보이저 1호, Time: 348 ms, Memory: 219620 KB -BaekjoonHub

--- 백준/Gold/3987. 보이저 1호/보이저 1호.py
+++ 백준/Gold/3987. 보이저 1호/보이저 1호.py
@@ -1,4 +1,6 @@
 """
+주의할 실수 : visited 초기화 위치 => 방향마다 필요;;
+
 블랙홀 C
 빈칸 .
 행성 \/       => 어떤 모양을 어느방향에서 만나는지에 따라 다음 방향 달라짐
@@ -6,6 +8,9 @@ PR, PC로 주어짐 초기위치
 가장 오랜 시간 머무는 방향과 시간
 여러개라면 URDL 순서 중에 앞서는 것
 무한하다면 Voyager
+
+리팩토링 : 어차피 방향 4개인데 set 쓸필요 없음
+
 """
 N, M = map(int, input().split())
 ARR = [list(input()) for _ in range(N)]
@@ -25,8 +30,8 @@ ans = 0  # 무한 -1 voyager
 ans_d = ""
 for sd in range(4):
     cr, cc, cd = sr - 1, sc - 1, sd  # 그림 상 좌표를 1, 1부터 시작하게 주고 있음
-    visited = [[set() for _ in range(M)] for _ in range(N)] # (D) visited 초기화 위치 생각하자 제발
-    visited[cr][cc].add(cd)
+    visited = [[[0]*4 for _ in range(M)] for _ in range(N)] # (D) visited 초기화 위치 생각하자 제발
+    visited[cr][cc][cd] = 1
     sec = 0
     while True:  # 나가거나, 왔던 위치에 동일한 방향이면 종료
         # print(cr, cc, cd)
@@ -41,11 +46,11 @@ for sd in range(4):
         if ARR[nr][nc] in ["/", "\\"]:
             cd = ND[ARR[nr][nc]][cd]
         cr, cc = nr, nc
-        if cd in visited[cr][cc]:  # 해당위치에 같은 방향으로 다시 왔다면 무한으로 voyager
+        if visited[cr][cc][cd] == 1:  # 해당위치에 같은 방향으로 다시 왔다면 무한으로 voyager
             ans = -1
             ans_d = to_char[sd]
             break
-        visited[cr][cc].add(cd)
+        visited[cr][cc][cd] = 1
 
     if ans == -1:  # 무한이면 나머지는 볼 필요도 없음
         break
