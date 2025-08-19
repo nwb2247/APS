2a01785287068c4dd76d15d72e029ffe637b7c0a
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-19 15:35:45 +0900

    [Gold IV] Title: 테트로미노, Time: 1376 ms, Memory: 113236 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -0,0 +1,64 @@
+"""
+회전 4 * 대칭 3 가지 * 나의 위치 4가지 * 테트로노미노 모양 5 => 160가지
+N*M = 4*500
+완탐 시간 OK
+
+좌측 상단을 0, 0으로 두고, 대칭 회전 시켜서 8개 모양만들기,
+각 모양에서 한점을 잡았을때 나머지의 상대위치 변환 시켜주는 함수
+4개가 전부 범위에서 벗어나는지 아닌지 확인
+"""
+tetros = [[(0, 0), (0, 1), (0, 2), (0, 3)],
+          [(0, 0), (1, 0), (2, 0), (2, 1)],
+          [(0, 0), (1, 0), (1, 1), (2, 1)],
+          [(0, 0), (0, 1), (0, 2), (1, 1)],
+          [(0, 0), (0, 1), (1, 0), (1, 1)]]
+# tetros = [[(0, 0), (1, 0), (2, 0), (2, 1)]]
+# 5개
+
+# 회전
+new = []
+for t in tetros:
+    tmp = [(c, r) for r, c in t]
+    new.append(tmp)
+tetros += new
+# 10개 (180도)
+
+# 대칭
+new = []
+for t in tetros:
+    tmp = [(-r, c) for r, c in t]
+    new.append(tmp)
+    tmp = [(r, -c) for r, c in t]
+    new.append(tmp)
+    tmp = [(-r, -c) for r, c in t]
+    new.append(tmp)
+tetros += new
+# 40개
+
+new = []
+for t in tetros:
+    for br, bc in t:
+        tmp = []
+        for r, c in t:
+            tmp.append((r-br, c-bc))
+        new.append(tmp)
+tetros = new # (new에 tetro가 포함되어있음)
+# 160개
+
+N, M = map(int, input().split())
+ARR = [list(map(int, input().split())) for _ in range(N)]
+ans = 0
+for cr in range(N):
+    for cc in range(M):
+        for tetro in tetros:
+            sm = 0
+            for tr, tc in tetro:
+                nr, nc = cr+tr, cc+tc
+                if not (0<=nr<N and 0<=nc<M):
+                    break # 이 테트로는 망함, sm 쓸모 없음
+                sm += ARR[nr][nc]
+            else:
+                # break 되지 않았다면 범위내 있는 테트로 이므로 ans에 반영
+                ans = max(ans, sm)
+print(ans)
+
