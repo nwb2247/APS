a56927f41af355443b999339b3d600a4e7048e73
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-20 20:38:50 +0900

    [Gold IV] Title: 테트로미노, Time: 156 ms, Memory: 114752 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,20 +1,66 @@
 """
+DFS 응용 - "특정한 수의" + "서로 인접한 칸으로 구성된" 블럭
+    [1] 시작점을 지정해서 BFS 진행
+    [2] 시작점에 대해선 방문 원상복구 안함
+        시작점에 대해서 가능한 모든 경우를 다 따져본것 이므로 굳이 다시 넣을 필요가 없음
+        즉 시작점이 포함되는 것은 이미 다 따져본 것이고, 앞으로 탐색할 것에 대해서는 시작점이 들어가지 않음
+    [3] 단, 한 시작점에 대해서는 여러개가 들어갈 수는 있음
+        따라서 "경우의 수"를 따지는 경우엔
+        정렬 -> set 연산 위한 튜플화 -> 중복 제거 의 처리가 필요함
 
-DFS 응용 -> "지금까지 추가한 것들과 맞닿아 있는 모든 것들을 후보로 넣으려면?!"
+(경우의 수 X)
+for sr in
+    for sc in
+        visited[sr][sc] = 1
+        backtrack(depth, [(sr, sc)]
+        # 원상 복구 X
 
-폴리오미노, N개의 정사각형들이 인접하게 붙어있어야함
+def backtrack(depth, contained):
+    if depth == K: # 정해진 블록의 수
+        (contained 에 대한 정답 처리)
+        return
 
-[1]
-이를 위해 현재까지 포함된 블록의 리스트를 유지한채로
-그 안에서 하나씩 꺼내 인접한 곳에 넣을 것이 있는지 확인하기!!!
+    for cr, cc in contained:
+        for dr, dc in DS:
+            nr, nc = cr+dr, cc+dc
+            if oob(nr, nc) : contained
+            if visited[nr][nc] == 0:
+                visited[cr][cc] = 1
+                backtrack(depth + 1, contained + [(nr, nc)]
+                visited[cr][cc] = 0
 
-[2]
-[1]을 수행하기 위해 시작점을 정해서 들어가는데
-일단 한점에서 돌리면
-그 점에서 들어갈 모든 경우의 수를 탐색하기 때문에
-다음번엔 그 점이 포함되는 경우는 고려할 필요가 없음
-    => 시작점에 대해서는 visited를 원상복구하지 않아도 됨
+--------------------------------------------------
+
+(경우의 수 O) ### : 추가된 부분
+cnt = 0 # 모든 경우의 수
+for sr in
+    for sc in
+        ### blocks = []
+        # 한 시작점에 대해선 중복이 있을 수 있기 때문에
+        # 튜플화 하여 넣고 set 만들어서 고유한 것만 남기기
+        visited[sr][sc] = 1
+        backtrack(depth, [(sr, sc)]
+        # 원상 복구 X
+
+        ### cnt += len(set(blocks))
+
+def backtrack(depth, contained):
+    if depth == K: # 정해진 블록의 수
+        ### blocks.append(tuple(sorted(contained)))
+        # 정렬하고, 튜플만들고 blocks에 넣기
+        return
+
+    for cr, cc in contained:
+        for dr, dc in DS:
+            nr, nc = cr+dr, cc+dc
+            if oob(nr, nc) : contained
+            if visited[nr][nc] == 0:
+                visited[cr][cc] = 1
+                backtrack(depth + 1, contained + [(nr, nc)]
+                visited[cr][cc] = 0
+
 """
+
 def oob(r, c):
     return not (0<=r<N and 0<=c<M)
 
@@ -62,77 +108,3 @@ for sr in range(N):
         # 따라서 VISITED[sr][sc] = 0 의 원상복구 안하는게 더 빠름
 
 print(ANS)
-
-
-# --------- 1번째 아이디어 : 모양의 경우의 수 만들어 완전탐색 ---------
-# """
-# 회전 4 * 대칭 3 가지 * 나의 위치 4가지 * 테트로노미노 모양 5 => 160가지
-# (D) 생각해보면 나의 위치 4는 곱할 필요가 없음
-# why? 어차피 다른 점에서 시작했을떄 내가 포함되는 경우가 있기때문 => 40가지면 충분
-# N*M = 4*500
-# 완탐 시간 OK
-#
-# 좌측 상단을 0, 0으로 두고, 대칭 회전 시켜서 8개 모양만들기,
-# 각 모양에서 한점을 잡았을때 나머지의 상대위치 변환 시켜주는 함수
-# 4개가 전부 범위에서 벗어나는지 아닌지 확인
-# """
-# tetros = [((0, 0), (0, 1), (0, 2), (0, 3)), # 중복 처리 위해 tuple로 받음 (이중 튜플도 set에 넣을 수 있음)
-#           ((0, 0), (1, 0), (2, 0), (2, 1)),
-#           ((0, 0), (1, 0), (1, 1), (2, 1)),
-#           ((0, 0), (0, 1), (0, 2), (1, 1)),
-#           ((0, 0), (0, 1), (1, 0), (1, 1))]
-# # 5개
-#
-# # 회전 (180도)
-# new = []
-# for t in tetros:
-#     tmp = tuple([(c, r) for r, c in t])
-#     new.append(tmp)
-# tetros += new
-# # 10개 (180도)
-#
-# # 대칭
-# new = []
-# for t in tetros:
-#     tmp = tuple([(-r, c) for r, c in t])
-#     new.append(tmp)
-#     tmp = tuple([(r, -c) for r, c in t])
-#     new.append(tmp)
-#     tmp = tuple([(-r, -c) for r, c in t])
-#     new.append(tmp)
-# tetros += new
-# # 40개
-# sset = set(tetros)
-# # len(sset) # 36개
-# tetros = list(sset)
-# # for t in tetros:
-# #     print(t)
-#
-#
-# # new = []                           (D) 어차피 다른 점 시작에서 내가 포함될 수 있으므로 이부분은 굳이 X
-# # for t in tetros:
-# #     for br, bc in t:
-# #         tmp = []
-# #         for r, c in t:
-# #             tmp.append((r-br, c-bc))
-# #         new.append(tuple(tmp))
-# # tetros = new # (new에 tetro가 포함되어있음)
-# # # 160개
-#
-# N, M = map(int, input().split())
-# ARR = [list(map(int, input().split())) for _ in range(N)]
-# ans = 0
-# for cr in range(N):
-#     for cc in range(M):
-#         for tetro in tetros:
-#             sm = 0
-#             for tr, tc in tetro:
-#                 nr, nc = cr+tr, cc+tc
-#                 if not (0<=nr<N and 0<=nc<M):
-#                     break # 이 테트로는 망함, sm 쓸모 없음
-#                 sm += ARR[nr][nc]
-#             else:
-#                 # break 되지 않았다면 범위내 있는 테트로 이므로 ans에 반영
-#                 ans = max(ans, sm)
-# print(ans)
-#
