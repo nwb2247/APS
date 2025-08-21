412ed5cd0379ecae99c0b1d253af56f0f6c6d63b
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-21 14:19:09 +0900

    [Gold V] Title: 로봇 청소기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -0,0 +1,88 @@
+"""
+처음 방향과 위치 주어짐
+0북 1동 2남 3서
+0 청소 X
+1 or oob 벽
+벽과 청소 O 구별해야함 청소 O => 2
+
+S. 현재칸 미청소시 청소
+2. 4칸 중 미청소 없는 경우
+    1. 벽이 아니라면
+        바라보는 방향 유지한 채로 후진 (cd+2)%4 -> S로 돌아감
+    2. 벽이라면 작동 멈춤
+3. 빈칸이 있는 경우
+    1.반시계 방향으로 회전 (cd + 4 - 1)%4
+        1. 바라보는 앞 칸이 청소되지 않은 경우 한칸 전진
+    S로 돌아감
+
+"""
+N, M = map(int, input().split())
+cr, cc, cd = map(int, input().split())
+ARR = [list(map(int, input().split())) for _ in range(N)]
+
+DIR = {0:"북", 1:"동", 2:"남", 3:"서"}
+DS = [(-1, 0), (0, 1), (1, 0), (0, -1)] # 0북 1동 2남 3서
+# for l in ARR:
+#     print(l)
+
+def oob(r, c):
+    return not (0<=r<N and 0<=c<M)
+
+cnt = 0
+while True: # 0청소X 1벽 2청소O
+
+    # print(cr, cc, cd)
+    # ARRR = [lst[:] for lst in ARR]
+    # ARRR[cr][cc] = DIR[cd]
+    # for l in ARRR:
+    #     print(*l)
+    # print()
+
+
+    if ARR[cr][cc] == 0:
+        cnt += 1
+        ARR[cr][cc] = 2
+
+    is_zero = False
+    for dr, dc in DS:
+        nr, nc = cr+dr, cc+dc
+        if not oob(nr, nc) and ARR[nr][nc] == 0:
+            is_zero = True
+            break
+
+    if not is_zero:
+        nd = (cd+2)%4
+        dr, dc = DS[nd]
+        nr, nc = cr+dr, cc+dc
+        if not oob(nr, nc) and ARR[nr][nc] != 1:
+            # cr, cc, cd = nr, nc, nd # (D) "방향을 유지한 채로"
+            cr, cc = nr, nc
+            continue
+        else:
+            break
+    else:
+        nd = (cd + 4 - 1)%4
+        cd = nd         # 회전은 무조건 해야함
+        dr, dc = DS[nd]
+        nr, nc = cr+dr, cc+dc
+        if ARR[nr][nc] == 0:
+            cr, cc = nr, nc
+        # else : continue
+
+
+print(cnt)
+
+
+
+"""
+S. 현재칸 미청소시 청소
+2. 4칸 중 미청소 없는 경우
+    1. 벽이 아니라면
+        "바라보는 방향 유지한 채로" 후진 (cd+2)%4 -> S로 돌아감
+    2. 벽이라면 작동 멈춤
+3. 빈칸이 있는 경우
+    1.반시계 방향으로 회전 (cd + 4 - 1)%4
+        1. 바라보는 앞 칸이 청소되지 않은 경우 한칸 전진
+    S로 돌아감
+"""
+
