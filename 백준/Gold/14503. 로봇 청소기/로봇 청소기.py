e5f3ad610baae5f094e664009eb6733aefbbf05c
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-21 14:41:45 +0900

    [Gold V] Title: 로봇 청소기, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -7,13 +7,16 @@
 
 S. 현재칸 미청소시 청소
 2. 4칸 중 미청소 없는 경우
-    1. 벽이 아니라면
-        바라보는 방향 유지한 채로 후진 (cd+2)%4 -> S로 돌아감
-    2. 벽이라면 작동 멈춤
-3. 빈칸이 있는 경우
-    1.반시계 방향으로 회전 (cd + 4 - 1)%4
-        1. 바라보는 앞 칸이 청소되지 않은 경우 한칸 전진
-    S로 돌아감
+    2-1. 벽이 아니라면
+        바라보는 방향 유지한 채로 후진 (cd+2)%4
+        S로 돌아감 (continue)
+    2-2. 벽이라면
+        작동 멈춤
+3. 미청소 있는 경우
+    반시계 방향으로 회전 (cd + 4 - 1)%4
+        3-1. 바라보는 앞 칸이 청소되지 않은 경우
+            한칸 전진
+    S로 돌아감 (continue)
 
 """
 N, M = map(int, input().split())
@@ -31,6 +34,7 @@ def oob(r, c):
 cnt = 0
 while True: # 0청소X 1벽 2청소O
 
+    # 디버깅용
     # print(cr, cc, cd)
     # ARRR = [lst[:] for lst in ARR]
     # ARRR[cr][cc] = DIR[cd]
@@ -39,50 +43,35 @@ while True: # 0청소X 1벽 2청소O
     # print()
 
 
-    if ARR[cr][cc] == 0:
+
+
+    if ARR[cr][cc] == 0:                                # S. 현재칸 미청소시 청소
         cnt += 1
         ARR[cr][cc] = 2
 
-    is_zero = False
+    is_zero = False                                     # 인접 네 칸 확인해 미청소된 구역 있는지 확인
     for dr, dc in DS:
         nr, nc = cr+dr, cc+dc
         if not oob(nr, nc) and ARR[nr][nc] == 0:
             is_zero = True
             break
 
-    if not is_zero:
+    if not is_zero:                                     #2. 4칸 중 미청소 없는 경우
         nd = (cd+2)%4
         dr, dc = DS[nd]
         nr, nc = cr+dr, cc+dc
-        if not oob(nr, nc) and ARR[nr][nc] != 1:
-            # cr, cc, cd = nr, nc, nd # (D) "방향을 유지한 채로"
+        if not oob(nr, nc) and ARR[nr][nc] != 1:                # 2-1. 벽이 아니라면
+            # cr, cc, cd = nr, nc, nd # (D) "방향을 유지한 채로"   # 바라보는 방향 유지한 채로 후진 (cd+2)%4 + 돌아감 continue
             cr, cc = nr, nc
             continue
-        else:
-            break
-    else:
-        nd = (cd + 4 - 1)%4
-        cd = nd         # 회전은 무조건 해야함
+        else:                                                   # 2-2. 벽이라면
+            break                                               # 작동 멈춤 (loop 탈출)
+    else:                                               #3. 미청소 있는 경우
+        nd = (cd + 4 - 1)%4                                     # 반시계 방향으로 회전 (cd + 4 - 1)%4
+        cd = nd                                                 # 회전은 무조건 해야함
         dr, dc = DS[nd]
         nr, nc = cr+dr, cc+dc
-        if ARR[nr][nc] == 0:
-            cr, cc = nr, nc
-        # else : continue
-
+        if ARR[nr][nc] == 0:                                    # 3-1. 바라보는 앞 칸이 청소되지 않은 경우
+            cr, cc = nr, nc                                         # 한칸 전진
 
 print(cnt)
-
-
-
-"""
-S. 현재칸 미청소시 청소
-2. 4칸 중 미청소 없는 경우
-    1. 벽이 아니라면
-        "바라보는 방향 유지한 채로" 후진 (cd+2)%4 -> S로 돌아감
-    2. 벽이라면 작동 멈춤
-3. 빈칸이 있는 경우
-    1.반시계 방향으로 회전 (cd + 4 - 1)%4
-        1. 바라보는 앞 칸이 청소되지 않은 경우 한칸 전진
-    S로 돌아감
-"""
-
