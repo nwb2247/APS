f77b00f8b496a5d08dc6b1555fc84350447317ac
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-25 20:44:37 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 444 ms, Memory: 134980 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -2,6 +2,13 @@
 3차원, 이동횟수는 q로 관리, v는 방문여부만 (방문했다는 것은 이미 최소거리로 도달했다는 뜻)
 +
 같은 k에서만 비교 (뭐가 더 빠를지?)
++
+H-1, W-1 도달했다면 break
+=> (1000ms로 단축)
+
+=> 그냥 최소로 방문한 k를 넣어버리자
+(k번만 사용하고도 최소비용으로 여기에 올 수 있다는 뜻이므로)
+단, 같은 dist인데 k가 더 작다면 같은 k로 갱신
 """
 from collections import deque
 
@@ -17,11 +24,11 @@ K = int(input())
 W, H = map(int, input().split())
 arr = [list(map(int, input().split())) for i in range(H)]
 
-v = [[[0 for _ in range(K + 1)] for _ in range(W)] for _ in range(H)]
+v = [[-1 for _ in range(W)] for _ in range(H)]
 dist = [[float('inf')] * W for _ in range(H)]
 
 q = deque()
-v[0][0][0] = 1
+v[0][0] = 0
 q.append((0, 0, 0, 0))  # cr, cc, k, sec
 
 ans = float('inf')
@@ -38,8 +45,8 @@ while q:
             if oob(nr, nc) or arr[nr][nc] == 1:
                 continue
 
-            if v[nr][nc][ck + 1] == 0:
-                v[nr][nc][ck + 1] = 1
+            if v[nr][nc] == -1 or v[nr][nc] > ck+1:
+                v[nr][nc] = ck+1
                 q.append((nr, nc, ck + 1, sec + 1))
 
     for dr, dc in monkey:
@@ -47,12 +54,66 @@ while q:
         if oob(nr, nc) or arr[nr][nc] == 1:
             continue
 
-        for i in range(ck + 1):
-            if v[nr][nc][i] != 0:
-                break
-        if v[nr][nc][ck] == 0:
-            v[nr][nc][ck] = 1
+        if v[nr][nc] == -1 or v[nr][nc] > ck:
+            v[nr][nc] = ck
             q.append((nr, nc, ck, sec + 1))
+
+# """
+# 3차원, 이동횟수는 q로 관리, v는 방문여부만 (방문했다는 것은 이미 최소거리로 도달했다는 뜻)
+# +
+# 같은 k에서만 비교 (뭐가 더 빠를지?)
+# => (1000ms로 단축)
+# """
+# from collections import deque
+#
+# monkey = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+# horse = [(-2, -1), (-1, -2), (2, -1), (1, -2), (2, 1), (1, 2), (-2, 1), (-1, 2)]
+#
+#
+# def oob(r, c):
+#     return not (0 <= r < H and 0 <= c < W)
+#
+#
+# K = int(input())
+# W, H = map(int, input().split())
+# arr = [list(map(int, input().split())) for i in range(H)]
+#
+# v = [[[0 for _ in range(K + 1)] for _ in range(W)] for _ in range(H)]
+# dist = [[float('inf')] * W for _ in range(H)]
+#
+# q = deque()
+# v[0][0][0] = 1
+# q.append((0, 0, 0, 0))  # cr, cc, k, sec
+#
+# ans = float('inf')
+# while q:
+#     cr, cc, ck, sec = q.popleft()
+#
+#     if (cr, cc) == (H - 1, W - 1):
+#         ans = min(ans, sec)
+#         break
+#
+#     if ck < K:
+#         for dr, dc in horse:
+#             nr, nc = cr + dr, cc + dc
+#             if oob(nr, nc) or arr[nr][nc] == 1:
+#                 continue
+#
+#             if v[nr][nc][ck + 1] == 0:
+#                 v[nr][nc][ck + 1] = 1
+#                 q.append((nr, nc, ck + 1, sec + 1))
+#
+#     for dr, dc in monkey:
+#         nr, nc = cr + dr, cc + dc
+#         if oob(nr, nc) or arr[nr][nc] == 1:
+#             continue
+#
+#         for i in range(ck + 1):
+#             if v[nr][nc][i] != 0:
+#                 break
+#         if v[nr][nc][ck] == 0:
+#             v[nr][nc][ck] = 1
+#             q.append((nr, nc, ck, sec + 1))
 
 # for l in v:
 #     print(v)
