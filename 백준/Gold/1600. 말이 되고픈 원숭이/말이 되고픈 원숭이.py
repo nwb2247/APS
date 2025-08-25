5bd9c53c4c3820d353d37a27aced0d60cbe61ccb
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-08-25 20:23:25 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 1076 ms, Memory: 158596 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -1,8 +1,9 @@
 """
-
+3차원, 이동횟수는 q로 관리, v는 방문여부만 (방문했다는 것은 이미 최소거리로 도달했다는 뜻)
++
+같은 k에서만 비교 (뭐가 더 빠를지?)
 """
 from collections import deque
-
 
 monkey = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 horse = [(-2, -1), (-1, -2), (2, -1), (1, -2), (2, 1), (1, 2), (-2, 1), (-1, 2)]
@@ -16,50 +17,188 @@ K = int(input())
 W, H = map(int, input().split())
 arr = [list(map(int, input().split())) for i in range(H)]
 
-v = [[[-1 for _ in range(K+1)] for _ in range(W)] for _ in range(H)]
+v = [[[0 for _ in range(K + 1)] for _ in range(W)] for _ in range(H)]
+dist = [[float('inf')] * W for _ in range(H)]
 
 q = deque()
-v[0][0][0] = 0
-q.append((0, 0, 0))
+v[0][0][0] = 1
+q.append((0, 0, 0, 0))  # cr, cc, k, sec
 
+ans = float('inf')
 while q:
-    cr, cc, ck = q.popleft()
+    cr, cc, ck, sec = q.popleft()
+
+    if (cr, cc) == (H - 1, W - 1):
+        ans = min(ans, sec)
+        break
 
     if ck < K:
         for dr, dc in horse:
-            nr, nc = cr+dr, cc+dc
+            nr, nc = cr + dr, cc + dc
             if oob(nr, nc) or arr[nr][nc] == 1:
                 continue
 
-            for i in range(ck+2):
-                if v[nr][nc][i] != -1 and v[nr][nc][i] <= v[cr][cc][ck] + 1:
-                    break
-            else:
-                v[nr][nc][ck+1] = v[cr][cc][ck] + 1
-                q.appendleft((nr, nc, ck+1))
+            if v[nr][nc][ck + 1] == 0:
+                v[nr][nc][ck + 1] = 1
+                q.append((nr, nc, ck + 1, sec + 1))
 
     for dr, dc in monkey:
-        nr, nc = cr+dr, cc+dc
+        nr, nc = cr + dr, cc + dc
         if oob(nr, nc) or arr[nr][nc] == 1:
             continue
 
-        for i in range(ck+1):
-            if v[nr][nc][i] != -1 and v[nr][nc][i] <= v[cr][cc][ck] + 1:
+        for i in range(ck + 1):
+            if v[nr][nc][i] != 0:
                 break
-        else:
-            v[nr][nc][ck] = v[cr][cc][ck] + 1 # (D) v[nr][nc][ck]를 v[nr][nc] 로 씀;;
-            q.append((nr, nc, ck))
+        if v[nr][nc][ck] == 0:
+            v[nr][nc][ck] = 1
+            q.append((nr, nc, ck, sec + 1))
 
 # for l in v:
 #     print(v)
 
-ans = float('inf')
-for i in range(K+1):
-    if v[H-1][W-1][i] != -1 and ans >= v[H-1][W-1][i]:
-        ans = v[H-1][W-1][i]
+# for l in v:
+#     print(l)
 
 if ans == float("inf"):
     print(-1)
 else:
     print(ans)
 
+# """
+# 3차원, 이동횟수는 q로 관리, v는 방문여부만 (방문했다는 것은 이미 최소거리로 도달했다는 뜻)
+# (3000ms)
+# """
+# from collections import deque
+#
+#
+# monkey = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+# horse = [(-2, -1), (-1, -2), (2, -1), (1, -2), (2, 1), (1, 2), (-2, 1), (-1, 2)]
+#
+#
+# def oob(r, c):
+#     return not (0 <= r < H and 0 <= c < W)
+#
+#
+# K = int(input())
+# W, H = map(int, input().split())
+# arr = [list(map(int, input().split())) for i in range(H)]
+#
+# v = [[[0 for _ in range(K+1)] for _ in range(W)] for _ in range(H)]
+# dist = [[float('inf')]*W for _ in range(H)]
+#
+# q = deque()
+# v[0][0][0] = 1
+# q.append((0, 0, 0, 0)) # cr, cc, k, sec
+#
+#
+# ans = float('inf')
+# while q:
+#     cr, cc, ck, sec = q.popleft()
+#
+#     if (cr, cc) == (H-1, W-1):
+#         ans = min(ans, sec)
+#         break
+#
+#     if ck < K:
+#         for dr, dc in horse:
+#             nr, nc = cr+dr, cc+dc
+#             if oob(nr, nc) or arr[nr][nc] == 1:
+#                 continue
+#
+#             for i in range(ck+2):
+#                 if v[nr][nc][i] != 0:
+#                     break
+#             else:
+#                 v[nr][nc][ck+1] = 1
+#                 q.append((nr, nc, ck+1, sec + 1))
+#
+#     for dr, dc in monkey:
+#         nr, nc = cr+dr, cc+dc
+#         if oob(nr, nc) or arr[nr][nc] == 1:
+#             continue
+#
+#         for i in range(ck+1):
+#             if v[nr][nc][i] != 0:
+#                 break
+#         else:
+#             v[nr][nc][ck] = 1
+#             q.append((nr, nc, ck, sec + 1))
+#
+# # for l in v:
+# #     print(v)
+#
+# # for l in v:
+# #     print(l)
+#
+# if ans == float("inf"):
+#     print(-1)
+# else:
+#     print(ans)
+
+
+# """
+# 3차원 visited (3000ms)
+# """
+# from collections import deque
+#
+#
+# monkey = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+# horse = [(-2, -1), (-1, -2), (2, -1), (1, -2), (2, 1), (1, 2), (-2, 1), (-1, 2)]
+#
+#
+# def oob(r, c):
+#     return not (0 <= r < H and 0 <= c < W)
+#
+#
+# K = int(input())
+# W, H = map(int, input().split())
+# arr = [list(map(int, input().split())) for i in range(H)]
+#
+# v = [[[-1 for _ in range(K+1)] for _ in range(W)] for _ in range(H)]
+#
+# q = deque()
+# v[0][0][0] = 0
+# q.append((0, 0, 0))
+#
+# while q:
+#     cr, cc, ck = q.popleft()
+#
+#     if ck < K:
+#         for dr, dc in horse:
+#             nr, nc = cr+dr, cc+dc
+#             if oob(nr, nc) or arr[nr][nc] == 1:
+#                 continue
+#
+#             for i in range(ck+2):
+#                 if v[nr][nc][i] != -1 and v[nr][nc][i] <= v[cr][cc][ck] + 1:
+#                     break
+#             else:
+#                 v[nr][nc][ck+1] = v[cr][cc][ck] + 1
+#                 q.append((nr, nc, ck+1))
+#
+#     for dr, dc in monkey:
+#         nr, nc = cr+dr, cc+dc
+#         if oob(nr, nc) or arr[nr][nc] == 1:
+#             continue
+#
+#         for i in range(ck+1):
+#             if v[nr][nc][i] != -1 and v[nr][nc][i] <= v[cr][cc][ck] + 1:
+#                 break
+#         else:
+#             v[nr][nc][ck] = v[cr][cc][ck] + 1 # (D) v[nr][nc][ck]를 v[nr][nc] 로 씀;;
+#             q.append((nr, nc, ck))
+#
+# # for l in v:
+# #     print(v)
+#
+# ans = float('inf')
+# for i in range(K+1):
+#     if v[H-1][W-1][i] != -1 and ans >= v[H-1][W-1][i]:
+#         ans = v[H-1][W-1][i]
+#
+# if ans == float("inf"):
+#     print(-1)
+# else:
+#     print(ans)
+#
