00d4d3f30adeead0a840ebaa2aed715f3916043f
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-09-15 20:56:26 +0900

    [Gold IV] Title: 인구 이동, Time: 796 ms, Memory: 123740 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,103 +1,168 @@
 """
-[한줄평]
-간단하게라도 손구상 꼭 하자
-손구상이 주석구상 보다 빠르게 정리가능, 간단하게 손코딩으로 구조 잡기
+[2차 풀이]
+1차 풀이와 달리 len으로 두개가 이어지는지 확인후 True, False 판단
 
-[타임라인]
-이해 및 구상 : 8분
-구현 : 10분
-디버깅 : 7분
------
-총 25분
+[이해 및 구상]
+인접한 두 계란 양 차이가 L이상 R이면 연결
 
-[구상]
-+) 손구상, 손코딩으로 구조 잡고 들어가기
-+) day의 출력 방법 잡고 들어간 것 나쁘지 않은듯
-+) L, R 조건 이상 이하 확실히 해둠
--) while 종료 조건을 좀만 더 확실하게 잡고 갔더라면..
-
-[구현]
-+) check 함수 good
--) lambda 사용법 잘 기억 안난다면 한번 시험해보거나 주석 처리해두면 좋을듯 (실수 방지)
--) 차분하게 주석도 적으면서 가기
-
-[디버깅]
-+) print, 디버거로 구상 부분에서 꼼꼼하지 못했던 종료조건 문제 캐치
-
-[시간복잡도]
-2000 * 50(N) * 50 ?
-
-[엣지케이스]
-(하루도 이동이 일어나지 않는 경우)
-2 10 10
-10 50
-20 30
-------
-0
-
+더 이상 이동이 없을 때까지 진행
 """
-
-
-# (손구상 요약)
-# L<= <=R 주의
-# 매 횟수 마다 visited 초기화
-# BFS로 연합찾고
-# pos에 담아주고 합 구해서 적용시켜줌
-# cnt 이용해서 연합이 더 이상 만들어지지 않으면 while break
-# 단 출력시 day-1를 출력 (마지막 날은 연합이 없어 이동이 이뤄지지 않은 날이므로)
-
 from collections import deque
-
-N, L, R = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-
-DS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
 def oob(r, c):
     return not (0 <= r < N and 0 <= c < N)
 
 
-def check(cr, cc, nr, nc):      # 연합 결성 L, R 조건 확인
-    return L <= abs(arr[cr][cc] - arr[nr][nc]) <= R
-
-
-day = 0
-while True:
-    day += 1
-    visited = [[0] * N for _ in range(N)]
-    cnt = 0 # (1이면 break)
+def check():
+    changed = False
+    v = [[0 for _ in range(N)] for _ in range(N)]  # 한 틀은 한그룹에만 들어감
     for sr in range(N):
         for sc in range(N):
-            if visited[sr][sc] == 0:
-                q = deque()
-                cnt += 1
-                pos = []
+            if v[sr][sc] != 0:  # 방문한 곳이면 안들어감
+                continue
+            v[sr][sc] = 1
+            q = deque()
+            q.append((sr, sc))
 
-                visited[sr][sc] = 1
-                q.append((sr, sc))
-                pos.append((sr, sc)) # (D) pos에도 넣어줘야함
-                
-                while q:
-                    cr, cc = q.popleft()
+            group = []
 
-                    for dr, dc in DS:
-                        nr, nc = cr+dr, cc+dc
-                        if oob(nr, nc):
-                            continue
-                        if visited[nr][nc] == 0 and check(cr, cc, nr, nc):
-                            visited[nr][nc] = 1
-                            pos.append((nr, nc))
-                            q.append((nr, nc))
+            while q:
+                cr, cc = q.popleft()
+                group.append((cr, cc))
 
-                sm = sum(map(lambda p:arr[p[0]][p[1]], pos)) # (D) : lambda는 자동 언패킹 안됨
-                for r, c in pos:
-                    arr[r][c] = sm//(len(pos))
+                for dr, dc in ds:
+                    nr, nc = cr + dr, cc + dc
+                    if oob(nr, nc) or not L <= abs(m[cr][cc] - m[nr][nc]) <= R or v[nr][nc] != 0:
+                        continue
+                    v[nr][nc] = 1
+                    q.append((nr, nc))
 
-    if cnt == N*N: # (D) 더 이상 두 국가가 연합을 이루지 않는 경우로 해야함 (처음에는 잘못생각해서 1로 둠;;;)
-        break
+            if len(group) > 1:
+                changed = True
+                sm = sum(map(lambda x: m[x[0]][x[1]], group))
+                for cr, cc in group:
+                    m[cr][cc] = sm // len(group)
 
-print(day-1)
+    return changed  # 바뀌는 것이 없다면 False 반환
 
 
+def solve():
+    cnt = 0
+    while check(): # 바뀌었다면 cnt += 1해줌 (바뀌지 않을때까지 계속)
+        cnt += 1
+        continue
+    print(cnt)
+    return
 
+
+ds = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+N, L, R = map(int, input().split())
+m = [list(map(int, input().split())) for _ in range(N)]
+
+solve()
+
+
+# ------------------------ 1차 풀이 -------------------------
+# """
+# [한줄평]
+# 간단하게라도 손구상 꼭 하자
+# 손구상이 주석구상 보다 빠르게 정리가능, 간단하게 손코딩으로 구조 잡기
+#
+# [타임라인]
+# 이해 및 구상 : 8분
+# 구현 : 10분
+# 디버깅 : 7분
+# -----
+# 총 25분
+#
+# [구상]
+# +) 손구상, 손코딩으로 구조 잡고 들어가기
+# +) day의 출력 방법 잡고 들어간 것 나쁘지 않은듯
+# +) L, R 조건 이상 이하 확실히 해둠
+# -) while 종료 조건을 좀만 더 확실하게 잡고 갔더라면..
+#
+# [구현]
+# +) check 함수 good
+# -) lambda 사용법 잘 기억 안난다면 한번 시험해보거나 주석 처리해두면 좋을듯 (실수 방지)
+# -) 차분하게 주석도 적으면서 가기
+#
+# [디버깅]
+# +) print, 디버거로 구상 부분에서 꼼꼼하지 못했던 종료조건 문제 캐치
+#
+# [시간복잡도]
+# 2000 * 50(N) * 50 ?
+#
+# [엣지케이스]
+# (하루도 이동이 일어나지 않는 경우)
+# 2 10 10
+# 10 50
+# 20 30
+# ------
+# 0
+#
+# """
+#
+# # (손구상 요약)
+# # L<= <=R 주의
+# # 매 횟수 마다 visited 초기화
+# # BFS로 연합찾고
+# # pos에 담아주고 합 구해서 적용시켜줌
+# # cnt 이용해서 연합이 더 이상 만들어지지 않으면 while break
+# # 단 출력시 day-1를 출력 (마지막 날은 연합이 없어 이동이 이뤄지지 않은 날이므로)
+#
+# from collections import deque
+#
+# N, L, R = map(int, input().split())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+#
+# DS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+#
+#
+# def oob(r, c):
+#     return not (0 <= r < N and 0 <= c < N)
+#
+#
+# def check(cr, cc, nr, nc):  # 연합 결성 L, R 조건 확인
+#     return L <= abs(arr[cr][cc] - arr[nr][nc]) <= R
+#
+#
+# day = 0
+# while True:
+#     day += 1
+#     visited = [[0] * N for _ in range(N)]
+#     cnt = 0  # (1이면 break)
+#     for sr in range(N):
+#         for sc in range(N):
+#             if visited[sr][sc] == 0:
+#                 q = deque()
+#                 cnt += 1
+#                 pos = []
+#
+#                 visited[sr][sc] = 1
+#                 q.append((sr, sc))
+#                 pos.append((sr, sc))  # (D) pos에도 넣어줘야함
+#
+#                 while q:
+#                     cr, cc = q.popleft()
+#
+#                     for dr, dc in DS:
+#                         nr, nc = cr + dr, cc + dc
+#                         if oob(nr, nc):
+#                             continue
+#                         if visited[nr][nc] == 0 and check(cr, cc, nr, nc):
+#                             visited[nr][nc] = 1
+#                             pos.append((nr, nc))
+#                             q.append((nr, nc))
+#
+#                 sm = sum(map(lambda p: arr[p[0]][p[1]], pos))  # (D) : lambda는 자동 언패킹 안됨
+#                 for r, c in pos:
+#                     arr[r][c] = sm // (len(pos))
+#
+#     if cnt == N * N:  # (D) 더 이상 두 국가가 연합을 이루지 않는 경우로 해야함 (처음에는 잘못생각해서 1로 둠;;;)
+#         break
+#
+# print(day - 1)
+#
+#
+#
