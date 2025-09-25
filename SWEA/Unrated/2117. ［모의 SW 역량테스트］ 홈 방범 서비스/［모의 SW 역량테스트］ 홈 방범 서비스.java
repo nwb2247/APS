4ec2f5dafa83d6ae497b5e5baae7f1045dba5a90
nwb2247 <147364004+nwb2247@users.noreply.github.com>
2025-09-25 09:22:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 254 ms, Memory: 38,720 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -1,73 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+def oob(r, c):
+    return not (0<=r<N and 0<=c<N)
 
-public class Solution {
-	
-	static int N, M;
-	static ArrayList<int[]> hList;
-	static int[] cost;
-	
-	static {
-		cost = new int[20+2]; // 20의 경우 짝수이므로 K=21까지 확인해야함
-		for (int k=1; k<20+2; k++) {
-			cost[k] = k*k+(k-1)*(k-1);
-		}
-	}
-	
-	static int sol = 0;
+def inside(cr, cc, br, bc, l):
+    return abs(cr-br) + abs(cc-bc) <= l
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		
-		int T = Integer.parseInt(br.readLine());
-		for (int t=1; t<=T; t++) {
-			
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			
-			hList = new ArrayList<>();
-			
-			for (int r=0; r<N; r++) {
-				st = new StringTokenizer(br.readLine());
-				for (int c=0; c<N; c++) {
-					int val = Integer.parseInt(st.nextToken());
-					if (val == 0) continue;
-					hList.add(new int[] {r,c});
-				}
-			}
-			
-			sol = 0;
-			
-			for (int r=0; r<N; r++) {
-				for (int c=0; c<N; c++) {
-					for (int k=1; k<=N+1; k++) { // k가 짝수인 경우 고려해서 N+1까지 확인
-						int cnt = 0;
-						for (int[] h : hList) {
-							if (Math.abs(r-h[0]) + Math.abs(c-h[1]) <= k-1) cnt++;
-						}
-						if (cnt*M < cost[k]) continue;
-						sol = Math.max(sol, cnt);
-					}
-				}
-			}
-			
-			sb.append("#").append(t).append(" ").append(sol).append("\n");
-			
-			
-		}
-		
-		System.out.println(sb.toString());
-		
-		
-		
+def solve():
+    global ans
+    for br in range(N):
+        for bc in range(N):
+            for k in range(2 * N):
+                cost = k * k + (k - 1) * (k - 1)
+                cnt = 0
+                l = k - 1
+                for cr in range(br - l, br + l + 1):
+                    for cc in range(bc - l, bc + l + 1):
+                        # 격자 밖이거나, 맨해튼 내에 있는게 아니거나, 집이 없다면 넘어감
+                        if oob(cr, cc) or not inside(cr, cc, br, bc, l) or mmap[cr][cc] == 0:
+                            continue
+                        cnt += 1
+                if cnt * M - cost >= 0:  # 손해를 보지 않는거니까 0도 가능
+                    ans = max(ans, cnt)
 
-	}
+    return
 
-}
+
+TC = int(input())
+for tc in range(1, TC + 1):
+    N, M = map(int, input().split())
+    mmap = [list(map(int, input().split())) for _ in range(N)]
+
+
+    ans = 0
+    solve()
+    print(f"#{tc} {ans}")
